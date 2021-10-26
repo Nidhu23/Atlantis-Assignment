08caa17bfd4a43c144e0003c20525082f4c649b8
nidhuna <nidhuna.n@privatecircle.co>
2021-10-27 01:32:49 +0530

    code refactored

--- pronunciable.py
+++ pronunciable.py
@@ -8,14 +8,14 @@ def get_all_combinations_possible_in_given_str(inp_string):
         all_possible_combinations.extend("".join(i) for i in itertools.permutations(inp_string, str_len))
 
     regex_pattern = re.compile("[^aeiouAEIOU]{0,2}[aeiouAEIOU]")
-    ret_list = []
+    ret_set = set()
     for possible_combination in all_possible_combinations:
         if regex_pattern.match(possible_combination):
-            ret_list.append(possible_combination)
-    return ret_list
+            ret_set.add(possible_combination)
+    return ret_set
 
 
 if __name__ == '__main__':
-    inp_word = input("Enter a word ")
+    inp_word = input("Enter a word ? ")
     sub_strings = get_all_combinations_possible_in_given_str(inp_word)
     print("Output {}".format(' '.join(sub_strings)))
