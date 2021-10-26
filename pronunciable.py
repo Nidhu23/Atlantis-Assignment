b2c9a69808694520c3e161cd3bceb53da178a3a2
nidhuna <nidhuna.n@privatecircle.co>
2021-10-27 01:04:43 +0530

    code to find all possible pronunciable substrings

---
+++ pronunciable.py
@@ -0,0 +1,21 @@
+import re
+import itertools
+
+
+def get_all_combinations_possible_in_given_str(inp_string):
+    all_possible_combinations = []
+    for str_len in range(2, len(inp_string) + 1):
+        all_possible_combinations.extend("".join(i) for i in itertools.permutations(inp_string, str_len))
+
+    regex_pattern = re.compile("[^aeiouAEIOU]{0,2}[aeiouAEIOU]")
+    ret_list = []
+    for possible_combination in all_possible_combinations:
+        if regex_pattern.match(possible_combination):
+            ret_list.append(possible_combination)
+    return ret_list
+
+
+if __name__ == '__main__':
+    inp_word = input("Enter a word ")
+    sub_strings = get_all_combinations_possible_in_given_str(inp_word)
+    print("Output {}".format(' '.join(sub_strings)))
